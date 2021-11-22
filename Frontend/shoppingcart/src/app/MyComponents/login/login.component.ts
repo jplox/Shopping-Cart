a1e88f958b06fb83d316809521312248edfca957
jplox <rpawar@innominds.com>
2021-11-22 20:22:42 +0530

    routing and loginpage validation

--- Frontend/shoppingcart/src/app/MyComponents/login/login.component.ts
+++ Frontend/shoppingcart/src/app/MyComponents/login/login.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  email = "";
+  valid = {
+    email : true,
+    }
   constructor() { }
 
   ngOnInit(): void {
   }
-
-}
+ validate(type:string):void{
+  const emailPattern = /\S+@\S+\.\S+/;
+  if (type === 'email') {
+    this.valid.email = emailPattern.test(this.email);
+  }
+ }
+ onkey(event:any , type:string){
+  if (type === "email"){
+    this.email = event.target.value;
+  }
+  this.validate(type)
+ }
+ }
--- Frontend/shoppingcart/src/app/app-routing.module.ts
+++ Frontend/shoppingcart/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './MyComponents/landing-page/landing-page.component';
+import { LoginComponent } from './MyComponents/login/login.component';
+import { PageNotFoundComponent } from './MyComponents/page-not-found/page-not-found.component';
+import { RegisterComponent } from './MyComponents/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component:LandingPageComponent},
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  { path: '**', component:PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
