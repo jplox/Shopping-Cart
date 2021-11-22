b78b98d0ca386ca5b30fd3ba4652345ec0e1d473
jplox <rpawar@innominds.com>
2021-11-22 20:13:09 +0530

    created components

--- Frontend/shoppingcart/src/app/app.module.ts
+++ Frontend/shoppingcart/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './MyComponents/landing-page/landing-page.component';
+import { LoginComponent } from './MyComponents/login/login.component';
+import { RegisterComponent } from './MyComponents/register/register.component';
+import { PageNotFoundComponent } from './MyComponents/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginComponent,
+    RegisterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
